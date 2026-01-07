ff7a8a1ab11b9c4a6be7ee6da9d66045fee44d2f
Shon Arun <ajkallidukil8173@gmail.com>
2026-01-08 00:33:32 +0530

    Add world_path variable

--- src/sam_bot_description/launch/display.launch.py
+++ src/sam_bot_description/launch/display.launch.py
@@ -14,6 +14,7 @@ def generate_launch_description():
     default_rviz_config_path = os.path.join(pkg_share, 'rviz', 'config.rviz')
     bridge_config_path = os.path.join(pkg_share, 'config', 'bridge_config.yaml')
     gz_spawn_model_launch_source = os.path.join(ros_gz_sim_share, "launch", "gz_spawn_model.launch.py")
+    world_path = os.path.join(pkg_share, 'world', 'my_world.sdf')
 
     static_tf = Node(
         package='tf2_ros',
