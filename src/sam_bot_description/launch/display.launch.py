216c8333191997726191d65d5eb1b2416de615b6
Shon Arun <ajkallidukil8173@gmail.com>
2026-01-08 00:28:46 +0530

    Add use_sim_time argument

--- src/sam_bot_description/launch/display.launch.py
+++ src/sam_bot_description/launch/display.launch.py
@@ -28,7 +28,7 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
     rviz_node = Node(
         package='rviz2',
@@ -41,6 +41,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(name='model', default_value=default_model_path, description='Absolute path to robot model file'),
         DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file'),
+        DeclareLaunchArgument(name='use_sim_time', default_value='True', description='Flag to enable use_sim_time'),
         robot_state_publisher_node,
         rviz_node,
         static_tf
