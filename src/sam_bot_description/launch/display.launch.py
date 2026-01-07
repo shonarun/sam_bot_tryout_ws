6d1bb990b4c2fc3867efccd97275d5b2d05baca8
Shon Arun <ajkallidukil8173@gmail.com>
2026-01-08 00:41:37 +0530

    Rearrange import statements

--- src/sam_bot_description/launch/display.launch.py
+++ src/sam_bot_description/launch/display.launch.py
@@ -1,12 +1,13 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
 from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-import os
-from ament_index_python.packages import get_package_share_directory
 from ros_gz_sim.actions import GzServer
 from ros_gz_bridge.actions import RosGzBridge
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     pkg_share = get_package_share_directory(package='sam_bot_description').find('sam_bot_description')
